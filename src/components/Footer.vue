a994c495d8d1a17b6d1245bb23ffab3d8c8f63ce
Joseph Cai <josephcai2022@gmail.com>
2023-06-05 01:09:35 -0400

    Fixed formatting and styling

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,27 +3,12 @@
 <template>
   <div class="footer">
     <div class="number">
-      <p>Questions? Call 1-857-039-732 ( Not a real number)</p>
+      <p>Need Help? Call 1-800-668-6868 (Kids help phone)</p>
     </div>
 
     <div class="links">
       <a href="">FAQ</a>
       <a href="">Help Center</a>
-      <a href="">Account</a>
-      <a href="">Media Center</a>
-      <a href="">Investor Relations</a>
-      <a href="">Jobs</a>
-      <a href="">Redeem Gift Cards</a>
-      <a href="">Buy Gift Cards</a>
-      <a href="">Ways to Watch</a>
-      <a href="">Terms of Use</a>
-      <a href="">Privacy</a>
-      <a href="">Cookie Preferences</a>
-      <a href="">Corporate Information</a>
-      <a href="">Contact Us</a>
-      <a href="">Speed Test</a>
-      <a href="">Legal Notices</a>
-      <a href="">Only on Not Netflix</a>
     </div>
   </div>
 </template>
